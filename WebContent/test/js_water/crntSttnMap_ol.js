bea5c357504533a345814d8cc74c1046b2ab5ef2
hyeokpark <hyeokpark92@gmail.com>
2019-03-15 18:05:12 +0900

    main2 관련 작업

---
+++ WebContent/test/js_water/crntSttnMap_ol.js
@@ -0,0 +1,37 @@
+﻿var _CrntSttnMap_ol = function () {
+	var preZoomLevel = 0;
+	
+	var init = function(){
+		setEvent();
+		_MapService.getWfs(':CITY','*').done(function(){
+			debugger;
+		});
+	};
+	
+	var setEvent = function(){
+		_MapEventBus.on(_MapEvents.map_moveend, function(){
+			var getZoom = _CoreMap.getMap().getView().getZoom();
+			if(preZoomLevel != getZoom){
+				preZoomLevel = getZoom;
+			}
+		});
+	};
+	
+	var writeLayer = function(){
+		
+	};
+	
+	var getData = function(url){
+		return $.ajax({
+			url:url,
+            type: 'GET',
+            contentType: 'application/json'
+		});
+	};
+	
+	return {
+		init: function () {
+			init();
+		}
+	};
+}();
